af8e181c83d06ea03b8f917ee520644a584dfe5a
Yaroslav Andriyenko <andriyenko.yaroslav@gmail.com>
2020-06-22 20:21:48 +0300

    Updates

--- precache-manifest.95179d963483a62aded461d59920fbb2.js
+++ precache-manifest.95179d963483a62aded461d59920fbb2.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "a605ec7051b37132c026acf4d7073f10",
+    "revision": "91ecfa0be893f019a21299f4db6323db",
     "url": "./index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "./static/css/2.c3343ac3.chunk.css"
   },
   {
-    "revision": "5921dbf1ca386c3eef6c",
+    "revision": "a164c81f7b983fcfd1d8",
     "url": "./static/css/main.694a8f7f.chunk.css"
   },
   {
@@ -20,8 +20,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "./static/js/2.1aed9dc4.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "5921dbf1ca386c3eef6c",
-    "url": "./static/js/main.43c7bc52.chunk.js"
+    "revision": "a164c81f7b983fcfd1d8",
+    "url": "./static/js/main.30c90655.chunk.js"
   },
   {
     "revision": "7859211cf5cc2a17445c",
